f0bf6e14378bcc8faf9b0848fdcc066c125f78c6
Andrew Galbraiith <andrew.galbraith@prolixity.se>
2023-09-21 06:19:23 +0200

    Graphs saved

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,12 +1,9 @@
 import numpy as np
-
 from io_functions import IOFunctions
 from linear_regression import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression as LinearRegressionSK
-from sklearn.linear_model import LinearRegression as LR
 import pandas as pd
-
 import matplotlib.pyplot as plt
 # To test
 from sklearn.metrics import mean_squared_error
@@ -22,7 +19,6 @@ print(df.head(10))
 
 # 2.1
 validation_proportion = 0.2
-
 validation = df.sample(frac=validation_proportion, random_state=42)
 train = df.drop(validation.index)
 a, b = LinearRegression.calculate_linear_regression(train, "age", "2020")
@@ -41,14 +37,16 @@ print(mse)
 
 # mse_scikit = mean_squared_error(augment_df["2020"], augment_df["Predicted"])
 # print(mse_scikit)
+
 # 2.5
 # MSE is hard to interpret and depends upon the scale of the expected y values.
 # However, this MSE is not terrible.
 
-# 3
+# Task 3
 
 df3 = IOFunctions.read_file_to_dataframe(fullset_filename)
 print(df3.head(10))
+
 # Deal with 100+
 df3["age"] = df3["age"].apply(lambda x: "100 years" if x == "100+ years" else x)
 df3["age"] = df3["age"].apply(lambda x: int(x.rsplit(' ', 1)[0]))
@@ -77,9 +75,13 @@ print(mse)
 
 # 3.6 Much higher MSE since this is a curve and not a simple linear.
 
+# Task 4
+
 # 4.1 Linear regression works well for linear graphs but not for curves.
 
 # 4.2 The prediction needs to be a polynomial.
+
+# Task 5
 
 # 5.1
 validation = validation.sort_values("age")
@@ -105,9 +107,11 @@ calculateRegressionAndMSE(3)
 calculateRegressionAndMSE(5)
 calculateRegressionAndMSE(8)
 
-# 5.3 ??
+# 5.3 Hyperparameter tuning is carried out with the validation set
 
 # 5.3  8 is best, since it results in the lowest MSE
+
+# 5.4
 poly_features = PolynomialFeatures(degree=8, include_bias=False)
 X_poly = poly_features.fit_transform(X)
 lin_reg = LinearRegressionSK()
@@ -116,8 +120,7 @@ y_lin_fit = lin_reg.predict(X_poly)
 df5 = pd.DataFrame(list(zip(X, y, y_lin_fit)), columns=["age", "2020", "predicted"])
 mse = LinearRegression.calculate_mse(df5, "2020", "predicted")
 
-# 5.4
-# TODO Better polynomial plot
+# TODO Better polynomial plot?
 plt.scatter(X, y, label='Data points')
 plt.plot(df3_grouped_by_age_group["age"], a + b * df3_grouped_by_age_group["age"], marker='x', label='Regression, x=1')
 plt.plot(X, y_lin_fit, marker='o', label='Polynomial, x=8')
