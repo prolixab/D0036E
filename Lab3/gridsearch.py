f939349b7901bc9f9edde286a44ba780dbccae71
Andrew Galbraith <galnd@jonkoping.se>
2023-10-07 12:57:04 +0200

    Grid search class

---
+++ Lab3/gridsearch.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+class GridSearch:
+
+    def __init__(self, a, b, step, func):
+        self.a = a
+        self.b = b
+        self.step = step
+        self.func = func
+        self.hyperparameter_list=[]
+        self.result_list=[]
+
+    def perform_search(self):
+        self.hyperparameter_list=[]
+        self.result_list=[]
+        for x in range(self.a, self.b, self.step):
+            self.hyperparameter_list.append(x)
+            result = self.func(x)
+            self.result_list.append(result)
+
+    def plot_results(self):
+        plt.plot(self.hyperparameter_list, self.result_list)
+        plt.xlabel('Hyperparameter')
+        plt.ylabel('Silhouette Coefficient')
+        plt.title('Silhouette Coefficient based on Hyperparameter')
+        plt.show()
