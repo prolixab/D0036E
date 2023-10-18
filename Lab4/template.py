6abae4971858b9d20a0cdeb0dbe5e0f8666d6a76
Andrew Galbraiith <andrew.galbraith@prolixity.se>
2023-10-18 07:48:51 +0200

    NP used.

--- Lab4/template.py
+++ Lab4/template.py
@@ -6,7 +6,7 @@ def match_template(main_image, template_image):
     template_image_y, template_image_x = template_image.shape
     rows = main_image_y - template_image_y
     columns = main_image_x - template_image_x
-    results_array = [[0 for x in range(columns)] for y in range(rows)]
+    results_array = np.array([[0 for x in range(columns)] for y in range(rows)])
     for i in range(rows):
         for j in range(columns):
             part_image = main_image[i:i + template_image_y, j:j + template_image_x]
@@ -15,13 +15,10 @@ def match_template(main_image, template_image):
 
 
 def calculate_value(part_image, template_image):
-    part_image_np=np.array(part_image)
-    template_image_np=np.array(template_image)
-    diff=part_image_np-template_image_np
-
-
+    diff=part_image-template_image
+    diff_sq=diff**2
     # for i in range(rows):
     #     for j in range(columns):
     #         diff = part_image[i][j] - template_image[i][j]
     #         sum += abs(diff)
-    return diff.sum()
+    return diff_sq.sum()
